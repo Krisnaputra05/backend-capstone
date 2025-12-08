68f7a7ed386e3bda3bc2f404ae365e0d9fe58051
Krisnaputra05 <krisnap770@gmail.com>
2025-12-08 19:00:19 +0800

    feat: Add initial Express server setup with core API routes and middleware.

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Halo Dunia! Ini Express.js pertamaku!");
+  res.send("Welcome to Capstone Project Management API");
 });
 
 app.use("/api/auth", authRouter);
